082cf24fd48963a0dec5f3195f2b73fbd5b176c1
maks <maxdabrowski93@o2.pl>
2019-10-24 14:40:33 +0200

    newsfilter front

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://maks:MD8253maksior4@cluster0-zzbwj.mongodb.net/test?retryWrites=true&w=majority',
     keySession: ['TWOJKLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000 
 }
