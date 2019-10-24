2c4b5c0f76471077934b782e22c0f4becde4836a
maks <maxdabrowski93@o2.pl>
2019-10-24 13:33:32 +0200

    admin login, news

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 var createError = require('http-errors');
+var cookieSession = require('cookie-session'); 
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config');
+var mongoose = require('mongoose');
+
+mongoose.connect(config.db, {useNewUrlParser: true, useUnifiedTopology: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection erroe:'));
+
 
 var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
 var quizRouter = require('./routes/quiz');
 var adminRouter = require('./routes/admin');
+
+
 
 var app = express();
 
@@ -20,6 +31,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name:'session',
+  keys: config.keySession,
+  maxAge: config.maxAgeSession,
+}))
 
 app.use(function(req,res,next){
   res.locals.path = req.path;
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,50 @@
 const express = require('express');
+const News = require('../models/news');
 const router = express.Router();
 
-/* GET home page. */
+
+router.all('*', (req, res, next) =>{
+  if(!req.session.admin){
+    res.redirect('login')
+    return;
+  }
+  next();
+})
+
 router.get('/', (req, res) => {
-  res.render('admin', { title: 'Admin' });
+  News.find({}, (err,data) => {
+    console.log(data)
+    res.render('admin/index', { title: 'Admin', data });
+  });
 });
+
+router.get('/news/add', (req,res) =>{
+res.render('admin/news-form', {title: 'Dodaj news', body:{}, errors:{}});
+});
+
+router.post('/news/add', (req,res) =>{
+  
+  const body = req.body;
+
+  const newsData = new News(body);
+  const errors = newsData.validateSync();
+  console.log(errors)
+  newsData.save((err) => {
+
+if(err){
+  res.render('admin/news-form', {title: 'Dodaj news', body, errors})
+return;
+  }
+
+  res.redirect('/admin');
+  });
+  });
+
+  router.get('/news/delete/:id', (req,res) =>{
+    News.findByIdAndDelete(req.params.id, (err)=>{
+      res.redirect('/admin')
+    })
+    });
+
 
 module.exports = router;
