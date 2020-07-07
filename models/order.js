e2d4fa08b8ca6c4dc3e494587c239fa9c4c7f827
maks <maxdabrowski93@o2.pl>
2020-07-07 23:17:23 +0200

    customers w ordr model

--- models/order.js
+++ models/order.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var orderSchema = new Schema({
+  customers:{type: String},
   table:{type: String},
   pizza:{type: Array, required: true},
   pizzaSize:{type: Array, required: true},
